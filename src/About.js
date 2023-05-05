4b17bf90644ef2b1826c88c86988eb2799f5c93f
POOJA15GOYAL <poojagoyal1506@gmail.com>
2023-05-05 18:50:48 +0530

    initial commit

---
+++ src/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div class="about section">
+        <div class="section title">
+          <h1>ABOUT US</h1>
+        </div>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return ( 
+    <nav className='navbar'>
+        <div className='links'>
+         <a  href="/">Home</a>
+         <a href="/">About</a>
+         <a  href="/">Program Details</a>
+         <a href="/">Initiative</a>
+         <a href="/">Suppoters & Sponsers</a>
+         <a href="/">Team</a>
+         <a href="/">FAQs</a>
+         <a href="/"></a>
+        </div>
+    </nav>    
+     );
+}
+ 
+export default Navbar;
